c6890a7080cb6a194ec1fda86ec8042bc9df6f29
Anuj_vashist <vashistanuj730@gmail.com>
2023-08-01 10:35:19 +0530

    done

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+// ///// <!-- preloader -->
+
+var loader = document.getElementById("preloader");
+
+function myfuction() {
+    loader.style.display = "none";
+}
+
+// --- scrollRevelscript ---
+src="https://unpkg.com/scrollreveal"
+ScrollReveal().reveal('.card', { delay: 10 });
+ScrollReveal().reveal('.icons', { interval: 50 });
